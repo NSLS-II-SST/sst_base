bc5664cbdd7eb15fc05403b6e9a8ea6c28553998
SST Operator <xf07id1@xf07id-ws5.nsls2.bnl.local>
2021-10-06 17:18:57 -0400

    i400 modifications

--- bl_base/i400.py
+++ bl_base/i400.py
@@ -1,8 +1,12 @@
-from ophyd import Device, Component as Cpt, EpicsSignal, Signal
+from ophyd import Device, Component as Cpt, EpicsSignal, Signal, EpicsSignalRO
 
 
 class I400(Device):
     exposure_time = Cpt(EpicsSignal, ":PERIOD_MON", write_pv=":PERIOD_SP", kind="config")
-    curr_range = Cpt(EpicsSignal, ":RANGE_MON", write_pv=":SET_RANGE", kind="config")
-    cap_bin = Cpt(EpicsSignal, ":CAP_MON", write_pv=":CAP_SP", kind="config")
-    
+    set_range = Cpt(EpicsSignal, ":RANGE_BP")
+    range_mon = Cpt(EpicsSignalRO, ":RANGE_MON", kind="config")
+    cap_bin = Cpt(EpicsSignal, ":CAP_SP", kind="config")
+    err = Cpt(EpicsSignalRO, ":ERR")
+    sts = Cpt(EpicsSignalRO, ":STS")
+    clrerr = Cpt(EpicsSignal, ":EXECUTE_CLEAR")
+    i1 = Cpt(EpicsSignalRO, ":I1_MON")
--- setup.py
+++ setup.py
@@ -9,6 +9,5 @@ setup(
     description="Beamline hardware classes",
     install_requires=requirements,
     name="bl_base",
-    use_scm_version=True,
     packages=find_packages()
 )
