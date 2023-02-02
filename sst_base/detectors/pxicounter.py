c91ab9ac0fb84828e7ab5f63397f137a2a7a27e5
Conan Weiland <cweiland@bnl.gov>
2023-02-02 10:03:57 -0500

    fixed integration time config issue

--- sst_base/detectors/pxicounter.py
+++ sst_base/detectors/pxicounter.py
@@ -1,12 +1,12 @@
-from ophyd import Device, Component as Cpt, EpicsSignal, Signal
+from ophyd import Device, Component as Cpt, EpicsSignal, Signal, EpicsSignalRO
 from ophyd.status import SubscriptionStatus
 
 class PXITimer(Device):
     integration_time = Cpt(EpicsSignal, "IT:1]Time", name = "Integration Time",kind="config")
-    acquire = Cpt(EpicsSignal, ":1]Sts", name="Acquire Status", kind="config")
+    acquire = Cpt(EpicsSignal, ":1]Sts", name="Acquire Status", kind="omitted")
 
 class PXIScalar(Device):
-    timer = PXITimer("XF:07ID-BI[Timer",name="Timer")
+    timer = Cpt(PXITimer,"XF:07ID-BI[Timer",add_prefix=(),name="Timer")
     ch1 = Cpt(EpicsSignal, "Ch:1]Cnts", name="Channel 1", kind="hinted")
     ch2 = Cpt(EpicsSignal, "Ch:2]Cnts", name="Channel 2", kind="hinted")
     ch3 = Cpt(EpicsSignal, "Ch:3]Cnts", name="Channel 3", kind="hinted")
@@ -14,7 +14,8 @@ class PXIScalar(Device):
     ch5 = Cpt(EpicsSignal, "Ch:5]Cnts", name="Channel 5", kind="hinted")
     ch6 = Cpt(EpicsSignal, "Ch:6]Cnts", name="Channel 6", kind="hinted")
     ch7 = Cpt(EpicsSignal, "Ch:7]Cnts", name="Channel 7", kind="hinted")
-    update_time = Cpt(Signal,value=0.05,kind="config")
+    
+    #integration_time = timer.integration_time #Cpt(Sig,value=0.1,kind="config")
 
     def trigger(self):
         def check_value(*, old_value, value, **kwargs):
@@ -22,11 +23,15 @@ class PXIScalar(Device):
 
         wait_time = self.timer.integration_time.get()+0.2
 
-        status = SubscriptionStatus(self.timer.acquire,check_value,timeout=wait_time,run=False,settle_time=self.update_time.get())
+        status = SubscriptionStatus(self.timer.acquire,check_value,timeout=wait_time,run=False,settle_time=self.timer.integration_time.get())
         self.timer.acquire.set(1)
         return status
 
     def set_integration(self,t):
         self.timer.integration_time.set(t)
+
+   # def __init__(self,*args,**kwargs):
+    #    self.configuration_attrs = ['timer.integration_time']
+    #    super().__init__(*args,**kwargs)
 
 pxiScalar = PXIScalar("XF:07ID-BI[Counter:1-",name="PXI Scalar")
