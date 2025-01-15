d4b3c65fa9545f563663f8ccb4f4eb934bc5358d
PriyankaKetkarBNL <155195376+PriyankaKetkarBNL@users.noreply.github.com>
2025-01-15 12:58:47 -0500

    Update energy.py
    Added base_grating_to_250, base_grating_to_1200, and base_grating_to_rsoxs functions that were present in sst_hw (https://github.com/NSLS-II-SST/sst_hw/blob/master/sst_hw/energy.py) and are used by RSoXS station.

--- sst_base/energy.py
+++ sst_base/energy.py
@@ -831,3 +831,60 @@ class EnPos(PseudoPositioner):
 def base_set_polarization(pol, en):
     yield from bps.mv(en.polarization, pol)
     return 0
+
+
+def base_grating_to_250(mono_en, en):
+    type = mono_en.gratingx.readback.get()
+    if "250l/mm" in type:
+        print("the grating is already at 250 l/mm")
+        return 0  # the grating is already here
+    print("Moving the grating to 250 l/mm.  This will take a minute...")
+    yield from psh4.close()
+    yield from bps.abs_set(mono_en.gratingx, 2, wait=True)
+    # yield from bps.sleep(60)
+    # yield from bps.mv(mirror2.user_offset, 0.04) #0.0315)
+    # yield from bps.mv(grating.user_offset, -0.0874)#-0.0959)
+    # yield from bps.mv(en.m3offset, 7.90)
+    yield from bps.mv(mono_en.cff, 1.385)
+    yield from bps.mv(en, 270)
+    yield from psh4.open()
+    print("the grating is now at 250 l/mm signifigant higher order")
+    return 1
+
+
+def base_grating_to_1200(mono_en, en):
+    type = mono_en.gratingx.readback.get()
+    if "1200" in type:
+        print("the grating is already at 1200 l/mm")
+        return 0  # the grating is already here
+    print("Moving the grating to 1200 l/mm.  This will take a minute...")
+    yield from psh4.close()
+    yield from bps.abs_set(mono_en.gratingx, 9, wait=True)
+    # yield from bps.sleep(60)
+    # yield from bps.mv(mirror2.user_offset, 0.2044) #0.1962) #0.2052) # 0.1745)  # 8.1264)
+    # yield from bps.mv(grating.user_offset, 0.0769) #0.0687) # 0.0777) # 0.047)  # 7.2964)  # 7.2948)#7.2956
+    yield from bps.mv(mono_en.cff, 1.7)
+    # yield from bps.mv(en.m3offset, 7.791)
+    yield from bps.mv(en, 270)
+    yield from psh4.open()
+    print("the grating is now at 1200 l/mm")
+    return 1
+
+
+def base_grating_to_rsoxs(mono_en, en):
+    type = mono_en.gratingx.readback.get()
+    if "RSoXS" in type:
+        print("the grating is already at RSoXS")
+        return 0  # the grating is already here
+    print("Moving the grating to RSoXS 250 l/mm.  This will take a minute...")
+    yield from psh4.close()
+    yield from bps.abs_set(mono_en.gratingx, 10, wait=True)
+    # yield from bps.sleep(60)
+    # yield from bps.mv(mirror2.user_offset, 0.2044) #0.1962) #0.2052) # 0.1745)  # 8.1264)
+    # yield from bps.mv(grating.user_offset, 0.0769) #0.0687) # 0.0777) # 0.047)  # 7.2964)  # 7.2948)#7.2956
+    # yield from bps.mv(mono_en.cff, 1.7)
+    # yield from bps.mv(en.m3offset, 7.87)
+    yield from bps.mv(en, 270)
+    yield from psh4.open()
+    print("the grating is now at RSoXS 250 l/mm with low higher order")
+    return 1
