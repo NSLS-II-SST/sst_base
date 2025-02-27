93cf0dc253e7e2656b416b4c64d2509cd16804ab
Charles Titus <ctitus@bnl.gov>
2025-02-27 10:37:13 -0500

    Better TIFF writing to proposal directories through FileStore subclass in sst_base

--- sst_base/cameras.py
+++ sst_base/cameras.py
@@ -15,12 +15,44 @@ from ophyd import (
 from ophyd.areadetector.filestore_mixins import FileStoreTIFFIterativeWrite
 from ophyd import Component as Cpt
 from nslsii.ad33 import SingleTriggerV33, StatsPluginV33
+from nbs_bl.beamline import GLOBAL_BEAMLINE as bl
+from os.path import join
 
 
-class TIFFPluginWithFileStore(TIFFPlugin, FileStoreTIFFIterativeWrite):
+class TIFFPluginWithProposalDirectory(TIFFPlugin, FileStoreTIFFIterativeWrite):
     """Add this as a component to detectors that write TIFFs."""
 
-    pass
+    def __init__(self, *args, md, camera_name, write_template="%Y/%m/%d/", **kwargs):
+        write_path_template = f"/nsls2/data/sst/proposals/{md['cycle']}/{md['data_session']}/assets/{camera_name}"
+        write_path_template = join(write_path_template, write_template)
+        super().__init__(*args, write_path_template=write_path_template, **kwargs)
+        self.md = md
+        self.camera_name = camera_name
+        self.write_template = write_template
+
+    @property
+    def read_path_template(self):
+        self._read_path_template = (
+            f"/nsls2/data/sst/proposals/{self.md['cycle']}/{self.md['data_session']}/assets/{self.camera_name}"
+        )
+        self._read_path_template = join(self._read_path_template, self.write_template)
+        return super().read_path_template
+
+    @property
+    def write_path_template(self):
+        self._write_path_template = (
+            f"/nsls2/data/sst/proposals/{self.md['cycle']}/{self.md['data_session']}/assets/{self.camera_name}"
+        )
+        self._write_path_template = join(self._write_path_template, self.write_template)
+
+        return super().write_path_template
+
+    @property
+    def reg_root(self):
+        self._root = (
+            f"/nsls2/data/sst/proposals/{self.md['cycle']}/{self.md['data_session']}/assets/{self.camera_name}/"
+        )
+        return super().reg_root
 
 
 class TIFFPluginEnsuredOff(TIFFPlugin):
@@ -102,34 +134,54 @@ class StandardProsilicaV33(SingleTriggerV33, ProsilicaDetector):
         return {"fields": [self.stats1.total.name]}
 
 
-class StandardProsilicaWithTIFF(StandardProsilica):
-    tiff = Cpt(
-        TIFFPluginWithFileStore,
-        suffix="TIFF1:",
-        write_path_template="/nsls2/data/sst/assets/%Y/%m/%d/",
-        root="/nsls2/data/sst/assets",
-    )
+def StandardProsilicaWithTIFFFactory(*args, camera_name="", write_template="%Y/%m/%d/", **kwargs):
+
+    class StandardProsilicaWithTIFF(StandardProsilica):
+        tiff = Cpt(
+            TIFFPluginWithProposalDirectory,
+            suffix="TIFF1:",
+            md=bl.md,
+            camera_name=camera_name,
+            write_template=write_template,
+        )
+
+    return StandardProsilicaWithTIFF(*args, **kwargs)
 
 
-class StandardProsilicaWithTIFFV33(StandardProsilicaV33):
-    tiff = Cpt(
-        TIFFPluginWithFileStore,
-        suffix="TIFF1:",
-        write_path_template="/nsls2/data/sst/assets/%Y/%m/%d/",
-        root="/nsls2/data/sst/assets",
-    )
+def StandardProsilicaWithTIFFV33Factory(*args, camera_name="", write_template="%Y/%m/%d/", **kwargs):
 
-class ColorProsilicaWithTIFFV33(StandardProsilicaV33):
-    tiff = Cpt(
-        TIFFPluginWithFileStore,
-        suffix="TIFF1:",
-        write_path_template="/nsls2/data/sst/assets/%Y/%m/%d/",
-        root="/nsls2/data/sst/assets",
-    )
-    def describe(self):
-        res = super().describe()
-        # Patch: device has a color dimension that is not reported correctly
-        # by ophyd.
-        res["Sample Imager Detector Area Camera_image"]["shape"] = (*res["Sample Imager Detector Area Camera_image"]["shape"], 3)
-        res["Sample Imager Detector Area Camera_image"]["dtype_str"] = "|u1"
-        return res
+    class StandardProsilicaWithTIFFV33(StandardProsilicaV33):
+        tiff = Cpt(
+            TIFFPluginWithProposalDirectory,
+            suffix="TIFF1:",
+            md=bl.md,
+            camera_name=camera_name,
+            write_template=write_template,
+        )
+
+    return StandardProsilicaWithTIFFV33(*args, **kwargs)
+
+
+def ColorProsilicaWithTIFFV33Factory(*args, camera_name="", write_template="%Y/%m/%d/", **kwargs):
+
+    class ColorProsilicaWithTIFFV33(StandardProsilicaV33):
+        tiff = Cpt(
+            TIFFPluginWithProposalDirectory,
+            suffix="TIFF1:",
+            md=bl.md,
+            camera_name=camera_name,
+            write_template=write_template,
+        )
+
+        def describe(self):
+            res = super().describe()
+            # Patch: device has a color dimension that is not reported correctly
+            # by ophyd.
+            res["Sample Imager Detector Area Camera_image"]["shape"] = (
+                *res["Sample Imager Detector Area Camera_image"]["shape"],
+                3,
+            )
+            res["Sample Imager Detector Area Camera_image"]["dtype_str"] = "|u1"
+            return res
+
+    return ColorProsilicaWithTIFFV33(*args, **kwargs)
